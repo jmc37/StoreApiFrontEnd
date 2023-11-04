8406c7df84a1418f8e817f3f3061ea5e8ebddd8d
Joseph Chan <josephchan36@gmail.com>
2023-11-03 23:18:49 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../css/Footer.css'; // Import your CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-links">
+        <div className="footer-section">
+          <h4>Get to Know Us</h4>
+          <ul>
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press Releases</li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Connect with Us</h4>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-section">
+        <h4>Make Money with Us</h4>
+        <ul>
+          <li>Sell on Amazon</li>
+          <li>Sell under Amazon Accelerator</li>
+          <li>Become an Affiliate</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import ProductDisplay from "../components/ProductDisplay";
+import Footer from "../components/Footer"
 function HomePage({ userAuthenticated, user }) {
   return (
     <div>
       <Header userAuthenticated={userAuthenticated} user={user} />
       <ProductDisplay />
+      <Footer />
     </div>
   );
 }
