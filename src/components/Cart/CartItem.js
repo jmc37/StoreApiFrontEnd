d6f1d8c06e24b484905e13e4c1b947433973583a
Joseph Chan <josephchan36@gmail.com>
2023-11-06 21:51:44 -0800

    updated css for cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -3,10 +3,10 @@ function CartItem({ name, id, price, image, removeItem, quantity, subtractQuanti
 
   return (
     <div className="cart-item" id={id}>
-      <img className="item-image" src={image} alt={name} />
-      <h3 className="item-name">{name}</h3>
-      <h3 className="item-price">${price}</h3>
-      <div className="item-quantity">
+      <img className="cart-item-image" src={image} alt={name} />
+      <h3 className="cart-item-name">{name}</h3>
+      <h3 className="cart-item-price">${price}</h3>
+      <div className="cart-item-quantity">
       {subtractQuantity && (<button className="quantity-button" onClick={() => subtractQuantity(id)}>-</button>)}
         <h3>{quantity}</h3>  
         {subtractQuantity && (<button className="quantity-button" onClick={() => addQuantity(id)}>+</button>)}
