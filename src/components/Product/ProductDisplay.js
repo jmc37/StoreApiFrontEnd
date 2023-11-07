737c554589420f058334507c8b113de965f18b34
Joseph Chan <josephchan36@gmail.com>
2023-11-06 21:45:42 -0800

    implemented product page

--- src/components/Product/ProductDisplay.js
+++ src/components/Product/ProductDisplay.js
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
+import "../../css/Product/productDisplay.css"
+import axios from "axios";
 
-const ProductDisplay = () => {
-  //   localStorage.clear();
+function ProductDisplay (){
   let { itemId } = useParams();
+  const source = "https://store-api-flask-python-project.onrender.com/item/";
   const [cart, setCart] = useState(() => {
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
+  const [item, setItem] = useState(null);
+
+  const getItem = useCallback(async () => {
+    try {
+      const response = await axios.get(source + itemId);
+      setItem(response.data);
+    } catch (error) {
+      console.error("API Error:", error);
+    }
+  }, [itemId]);
+
+  useEffect(() => {
+    getItem();
+  }, [getItem]);
+
   const addToCart = (item) => {
     const updatedCart = {
       ...cart,
@@ -16,12 +33,21 @@ const ProductDisplay = () => {
     localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
+  if (item === null) {
+    return null; // Render nothing if item is still null
+  }
+
   return (
-    <div>
-      <h2>Product Details</h2>
-      <p>Item ID: {itemId}</p>
-      <button onClick={() => addToCart(itemId)}>Add To Cart</button>
+    <div className="product-container">
+      <div className="grid-container">
+        <h2 className="title">Product Details</h2>
+        <img className="item-image" src={item.image} alt={item.name} />
+        <h1 className="item-name">{item.name}</h1>
+        <p className="item-description">{item.description}</p>
+        <button className="add-button"onClick={() => addToCart(itemId)}>Add To Cart</button>
+      </div>
     </div>
   );
-};
+}
+
 export default ProductDisplay;
