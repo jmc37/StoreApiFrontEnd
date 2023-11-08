6ab6efc5430629a02d462feeb294d2bd4cb03529
Joseph Chan <josephchan36@gmail.com>
2023-11-08 13:00:47 -0800

    Added underline feature to header and cursor pointer to footer

--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -4,22 +4,22 @@ import "../../css/Navigation/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer-container">
-        <div className="footer-info">
-          <h4>Get to Know Us</h4>
-          <ul>
-            <li>About Us</li>
-            <li>Careers</li>
-            <li>Press Releases</li>
-          </ul>
-        </div>
-        <div className="footer-contact">
-          <h4>Connect with Us</h4>
-          <ul>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
-          </ul>
-        </div>
+      <div className="footer-info">
+        <h4>Get to Know Us</h4>
+        <ul>
+          <li>About Us</li>
+          <li>Careers</li>
+          <li>Press Releases</li>
+        </ul>
+      </div>
+      <div className="footer-contact">
+        <h4>Connect with Us</h4>
+        <ul>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
       <div className="footer-sales">
         <h4>Make Money with Us</h4>
         <ul>
