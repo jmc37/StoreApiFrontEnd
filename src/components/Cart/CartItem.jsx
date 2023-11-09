01189de36de9cfdc55d601f6b4c9bb9fbcbe59ca
Joseph Chan <josephchan36@gmail.com>
2023-11-08 22:02:57 -0800

    adjusted checkout form

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-function CartItem({ name, id, price, image, removeItem, quantity, subtractQuantity, addQuantity }) {
-
+function CartItem({
+  name,
+  id,
+  price,
+  image,
+  removeItem,
+  quantity,
+  subtractQuantity,
+  addQuantity,
+}) {
   return (
     <div className="cart-item" id={id}>
       <img className="cart-item-image" src={image} alt={name} />
       <h3 className="cart-item-name">{name}</h3>
       <h3 className="cart-item-price">${price}</h3>
       <div className="cart-item-quantity">
-      {subtractQuantity && (<button className="quantity-button" onClick={() => subtractQuantity(id)}>-</button>)}
-        <h3>{quantity}</h3>  
-        {subtractQuantity && (<button className="quantity-button" onClick={() => addQuantity(id)}>+</button>)}
+        {subtractQuantity && (
+          <button
+            className="quantity-button"
+            onClick={() => subtractQuantity(id)}
+          >
+            -
+          </button>
+        )}
+        <h3>{quantity}</h3>
+        {subtractQuantity && (
+          <button className="quantity-button" onClick={() => addQuantity(id)}>
+            +
+          </button>
+        )}
       </div>
       {removeItem && ( // Check if removeItem function exists
         <button className="remove-button" onClick={() => removeItem(id)}>
